dbb6e997b285507ff8188c88b57049a92edcbb8d
paolametinhoue <paolametinhoue@gmail.com>
2025-06-27 19:29:29 +0100

    deploy13

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import path from 'path'
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist',
+    outDir: 'build',
     rollupOptions: {
       output: {
         manualChunks(id) {
